40801cf5e0fccc5edc8bdd91b0ce09f7babc1fa3
AF0ns3ca <afonsecahdez@gmail.com>
2024-02-19 13:57:56 +0100

    dark mode localsotage

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -3,24 +3,42 @@
  * @GitHub https://github.com/AF0ns3ca/op-sass.git
  */
 
-document.getElementById("dark-mode").addEventListener("click", function () {
-  const btn = document.getElementById("dark-btn");
-  //aplicar la clase darkmode a todo el documento
-  document.body.classList.toggle("dark-mode");
+document.addEventListener("DOMContentLoaded", function () {
+  // Verificar si el modo oscuro está activado en el localStorage
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
 
-  if (window.location.pathname.includes("index.html")) {
+  // Función para activar o desactivar el modo oscuro
+  function toggleDarkMode() {
+    const btn = document.getElementById("dark-btn");
+    // Aplicar la clase darkmode a todo el documento
+    document.body.classList.toggle("dark-mode");
 
-    if (btn.src.includes("dark-mode")) {
-      btn.src = "./build/assets/imgs/svg/light-mode.svg";
+    // Cambiar la imagen del botón según el estado del modo oscuro
+    if (window.location.pathname.includes("index.html")) {
+
+      if (btn.src.includes("dark-mode")) {
+        btn.src = "./build/assets/imgs/svg/light-mode.svg";
+        localStorage.setItem("darkMode", "true");
+      } else {
+        btn.src = "./build/assets/imgs/svg/dark-mode.svg";
+        localStorage.setItem("darkMode", "false");
+      }
     } else {
-      btn.src = "./build/assets/imgs/svg/dark-mode.svg";
-    }
-  } else {
-    if (btn.src.includes("dark-mode")) {
-      btn.src = "../build/assets/imgs/svg/light-mode.svg";
-    } else {
-      btn.src = "../build/assets/imgs/svg/dark-mode.svg";
+      if (btn.src.includes("dark-mode")) {
+        btn.src = "../build/assets/imgs/svg/light-mode.svg";
+        localStorage.setItem("darkMode", "true");
+      } else {
+        btn.src = "../build/assets/imgs/svg/dark-mode.svg";
+        localStorage.setItem("darkMode", "false");
+      }
     }
   }
 
+  // Agregar el listener al botón
+  document.getElementById("dark-mode").addEventListener("click", toggleDarkMode);
+
+  // Si el modo oscuro estaba activado previamente, activarlo nuevamente
+  if (isDarkMode) {
+    toggleDarkMode();
+  }
 });
